4856a717d1fe2815c57aeb0db28c9a4ac3c68e0e
Niklas Reimer <niklas@nr205.de>
2020-06-30 12:13:57 +0200

    [scripts] add: use env db.password in dump info

--- core/src/main/scripts/dumpPortalInfo.pl
+++ core/src/main/scripts/dumpPortalInfo.pl
@@ -1,4 +1,8 @@
 #!/usr/bin/env perl
 require "../scripts/envSimple.pl";
 
-exec("$JAVA_HOME/bin/java -Xmx1524M -Dspring.profiles.active=dbcp -cp $cp -DPORTAL_HOME='$portalHome' org.mskcc.cbio.portal.scripts.DumpPortalInfo @ARGV");
+if ($ENV{DB_PASSWORD} eq "") {
+    exec("$JAVA_HOME/bin/java -Xmx1524M -Dspring.profiles.active=dbcp -cp $cp -DPORTAL_HOME='$portalHome' org.mskcc.cbio.portal.scripts.DumpPortalInfo @ARGV");
+} else {
+    exec("$JAVA_HOME/bin/java -Xmx1524M -Dspring.profiles.active=dbcp -cp $cp -Ddb.password=$ENV{DB_PASSWORD} -DPORTAL_HOME='$portalHome' org.mskcc.cbio.portal.scripts.DumpPortalInfo @ARGV");
+}
